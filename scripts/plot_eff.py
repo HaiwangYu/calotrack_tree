15e722e4532e78d3d5a02726be382fd330258a95
HaiwangYu <yuhw.pku@gmail.com>
2025-08-12 23:51:36 -0400

    partially runs

--- scripts/plot_eff.py
+++ scripts/plot_eff.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from util import *
 from sklearn.metrics import adjusted_rand_score
 from util import *
+from tqdm import tqdm
 
 
 bin_edges = np.linspace(0, 5, 26)  # Define bin edges
@@ -17,10 +18,12 @@ integrated_pt_all_hist = np.zeros(len(integrated_pt_bins) - 1, dtype=int)
 
 dummy_pt_bin_edges = np.linspace(0, 1000, 2)  # a dummy bin to include all particles
 unbinned_aris = [ [] for _ in range(len(dummy_pt_bin_edges) - 1)]
-
+# Initialize tqdm for the event processing loop
+event_range = range(0, 9000)
+progress_bar = tqdm(event_range, desc="Processing events", unit="event")
 # Loop to read and process files from event_0 to event_10
-for ievent in range(0, 1000):
-    print (f"Processing event {ievent}...")
+for ievent in progress_bar:
+    # print (f"Processing event {ievent}...")
     # fname = f'2025-05-13-pp-1k-cut/data_event_{ievent}.h5'
     # fname = f'2025-05-20-pp-1k-nocut/data_event_{ievent}.h5'
     fname = f'2025-05-20-pp-10k-mincut/data_event_{ievent}.h5'
@@ -29,11 +32,11 @@ for ievent in range(0, 1000):
         seeds = store['seeds']            # Saved as fixed format
         particles = store['particles']    # Saved as fixed format
 
-        print(f"Number of particles: {len(particles)}")
+        # print(f"Number of particles: {len(particles)}")
         particles = particles[particles['cids'].apply(len) > 5]
-        print(f"|nclus| > 20: {len(particles)}")
+        # print(f"|nclus| > 20: {len(particles)}")
         particles = particles[particles['eta'].apply(abs) < 1.1]
-        print(f"|eta| < 1.1: {len(particles)}")
+        # print(f"|eta| < 1.1: {len(particles)}")
         # particles = particles[particles['vz'].apply(abs) < 10]
         # print(f"|vz| < 10: {len(particles)}")
         # particles = particles[particles['ptid']>0]
@@ -47,13 +50,13 @@ for ievent in range(0, 1000):
         all_hist, _ = np.histogram(all_pt, bins=bin_edges)
         total_matched_hist += matched_hist
         total_all_hist += all_hist
-        print(f"Event {ievent}: All {len(all_pt)} matched: {len(matched_pt)}\n")
+        # print(f"Event {ievent}: All {len(all_pt)} matched: {len(matched_pt)}\n")
 
         matched_hist, _ = np.histogram(matched_pt, bins=integrated_pt_bins)
         all_hist, _ = np.histogram(all_pt, bins=integrated_pt_bins)
         integrated_pt_matched_hist += matched_hist
         integrated_pt_all_hist += all_hist
-        print(f"Event {ievent}: All {len(integrated_pt_all_hist)} matched: {len(integrated_pt_matched_hist)}\n")
+        # print(f"Event {ievent}: All {len(integrated_pt_all_hist)} matched: {len(integrated_pt_matched_hist)}\n")
 
         # Calculate ARI
         cid_to_index = {cid: index for index, cid in enumerate(clusters['cid'])}
@@ -74,7 +77,7 @@ for ievent in range(0, 1000):
         for ibin in range(len(dummy_pt_bin_edges) - 1):
             ari = adjusted_rand_score(groupids_particle[ibin], groupids_seed[ibin])
             unbinned_aris[ibin].append(ari)
-            print(f"ievent {ievent}, bin {ibin}: unbinned ARI = {ari:.4f}")
+            # print(f"ievent {ievent}, bin {ibin}: unbinned ARI = {ari:.4f}")
 
 print("Total Matched histogram:", total_matched_hist)
 print("Total All histogram:", total_all_hist)
